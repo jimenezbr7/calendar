e40bf92be9bb86d83dc3bf52138525a53a36ea75
jimenezbr7 <jimenezbr7@gmail.com>
2017-11-08 19:21:54 -0500

    adding jquery

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-function goToNextMonth() {
-  var whatMonth = 
-}
+// function goToNextMonth() {
+  var whatMonthIsIt = $(".active").attr("id");
+
+  console.log(whatMonthIsIt);
+
+  // get the element with the class active
+  // check what the id on it is
+  // make decision on what next month
+  // MAKE SURE TO ADD CLASS TO THAT DIV AND REMOVE IT FROM THE OTHER
+
+// };
 
 
 
 
-document.getElementById("previous_month").addEventListener("click",goToNextMonth);
+// document.getElementById("next_month").addEventListener("click",goToNextMonth);
