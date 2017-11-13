6910ac851fdd531ddf6a86b389c533238a12ea95
jimenezbr7 <jimenezbr7@gmail.com>
2017-11-12 20:29:27 -0500

    updating the javascript

--- app.js
+++ app.js
@@ -63,31 +63,65 @@ function goToNextMonth() {
 function openNov13() {
   $("#wrapper").css("width", "1100px");
   $("#wrapper").css("margin", "0 auto");
-  $("#calendar_view").css("width", "50%");
+  $("#calendar_view").css("width", "49%");
   $("#daily_view").css("width", "50%");
   $("#calendar_view").css("float", "left");
   $("#daily_view").css("float", "left");
+  $("#calendar_view").css("font-size", "18px");
+  $("#daily_view").css("font-size", "18px");
+  $("#month_section").css("height", "70px")
+  $("#month_section").css("line-height", "70px")
+  $("#calendar_view").css("border", "black solid 1px");
   $("#nov13daily").show();
   $("#nov14daily").hide();
+  $("#nov15daily").hide();
   $("#nov13").css("color", "firebrick");
   $("#nov14").css("color", "white");
+  $("#nov15").css("color", "white");
 
 }
 
 function openNov14() {
   $("#wrapper").css("width", "1100px");
   $("#wrapper").css("margin", "0 auto");
-  $("#calendar_view").css("width", "50%");
+  $("#calendar_view").css("width", "49%");
   $("#daily_view").css("width", "50%");
   $("#calendar_view").css("float", "left");
   $("#daily_view").css("float", "left");
+  $("#calendar_view").css("font-size", "18px");
+  $("#daily_view").css("font-size", "18px");
+  $("#month_section").css("height", "70px");
+  $("#month_section").css("line-height", "70px");
+  $("#calendar_view").css("border", "black solid 1px");
   $("#nov14daily").show();
   $("#nov13daily").hide();
+  $("#nov15daily").hide();
   $("#nov14").css("color", "firebrick");
-  $("#nov13").css("color", "white");
+  $("#nov13").css("color", "white");    $("#nov15").css("color", "white");
+}
+
+function openNov15() {
+  $("#wrapper").css("width", "1100px");
+  $("#wrapper").css("margin", "0 auto");
+  $("#calendar_view").css("width", "49%");
+  $("#daily_view").css("width", "50%");
+  $("#calendar_view").css("float", "left");
+  $("#daily_view").css("float", "left");
+  $("#calendar_view").css("font-size", "18px");
+  $("#daily_view").css("font-size", "18px");
+  $("#month_section").css("height", "70px")
+  $("#month_section").css("line-height", "70px")
+  $("#calendar_view").css("border", "black solid 1px");
+  $("#nov15daily").show();
+  $("#nov14daily").hide();
+  $("#nov13daily").hide();
+  $("#nov15").css("color", "firebrick");
+  $("#nov13").css("color", "white");  $("#nov14").css("color", "white");
+
 }
 
 $("#previous_month").click(goToPriorMonth);
 $("#next_month").click(goToNextMonth);
 $("#nov13").click(openNov13);
 $("#nov14").click(openNov14);
+$("#nov15").click(openNov15);
